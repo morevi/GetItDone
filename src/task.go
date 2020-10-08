5f9d7f4960d8a475b7e50d0e1bbd4f0042088f6a
morevi <1998morevi@gmail.com>
2020-10-08 17:24:50 +0200

    Added first code

---
+++ src/task.go
@@ -0,0 +1,22 @@
+package task
+
+type Task struct {
+	checked bool
+	content string
+}
+
+func (t task) check () {
+}
+
+func (t task) setJob(text string) {
+}
+
+func (t task) getChecked() bool {
+	return t.checked
+}
+
+func (t Task) getJob() string {
+	return content
+}
+
+
