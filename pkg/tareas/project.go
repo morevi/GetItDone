dc6b10bf648fac501488fbc8f40d91623b5d7893
morevi <1998morevi@gmail.com>
2021-01-10 17:20:56 +0100

    Add Setter to Project, #2

--- pkg/tareas/project.go
+++ pkg/tareas/project.go
@@ -8,8 +8,38 @@ type Project struct {
   Items []Task
 }
 
-func (p *Project) New(tags []string, desc, todos []Task){
+func (p *Project) New(tags []string, desc, todos []Task) {
   p.Tags = tags
   p.Description = desc
   p.Items = todos
+}
+
+func (p *Project) SetDescription(nd string) {
+  p.Description = nd
+}
+
+func (p *Project) SetTags(newtags string) {
+  p.Tags = strings.Fields(newtags)
+}
+
+func(p *Project) AddTask(t Task) {
+  append(p.Items, t)
+}
+
+func(p *Project) RemoveTask(tpos int){
+  p.Items = append(p.Items[:tpos], p.Items[tpos:]...)
+}
+
+func(p *Project) GetAll() {
+  return p.Items
+}
+
+func(p *Project) GetDone(done bool) {
+  todos := []Task
+  for i, t := p.Items {
+    if t.Done == done {
+      append(todos, t)
+    }
+  }
+  return todos
 }
