f7f3197d0dfb0c41f786e98073158db4bf3001a3
morevi <1998morevi@gmail.com>
2021-01-10 16:06:52 +0100

    Add Task class declaration, #5 #19

--- pkg/tareas/task.go
+++ pkg/tareas/task.go
@@ -1,6 +1,20 @@
 package tareas
 
+//Class TASK
+import "time"
+
 type Task struct {
   Done bool
   Content string
+  Due time.Time
+}
+
+func (t Task) New(done bool, content string, due time.Time) {
+  t.Done = done
+  t.Content = content
+  t.Due = due
+}
+
+func (t Task) toogleCheck() {
+  t.Done = !(t.Done)
 }
