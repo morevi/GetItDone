52c7d31bdfcc30ef7fd95bd96e1576fc8f724502
morevi <1998morevi@gmail.com>
2021-01-13 15:40:52 +0100

    Test method New, continues #21

--- pkg/tareas/task_test.go
+++ pkg/tareas/task_test.go
@@ -3,7 +3,18 @@ package tareas
 
 import (
   "testing"
+  "time"
 )
 
 func TestNewTask(t *testing.T) {
+  done := false
+  desc := "test string"
+  due  := time.Now()
+
+  task := new(Task)
+  task.New(done, desc, due)
+
+  if task.Done != done || task.Content != desc || task.Due != due {
+    t.Error("Task.New() did not pass")
+  }
 }
