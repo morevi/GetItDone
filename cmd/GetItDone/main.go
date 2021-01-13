548d955ffa8ffb6acbad90466e023db120010faf
morevi <1998morevi@gmail.com>
2021-01-13 15:06:16 +0100

    Remove tests from main, #20

--- cmd/GetItDone/main.go
+++ cmd/GetItDone/main.go
@@ -1,17 +1,6 @@
 
 package main
 
-import (
-  "fmt"
-  "github.com/morevi/GetItDone/pkg/tareas"
-)
-
 func main() {
-  t := tareas.Task{false, "Subir commits a github"}
-
-  labels := []string{"Uni","IV","important"}
-  items := []tareas.Task{t}
-  l := tareas.TaskList{"Primer hito", labels, items}
-
-  fmt.Println(l.Name, l.Label, l.Items)
+  //todo
 }
