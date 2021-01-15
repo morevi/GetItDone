98d61b5caeadcbbab4130f9bcb89c92ceafb9d43
morevi <1998morevi@gmail.com>
2021-01-15 12:03:04 +0100

    Add simple get task by indexation method, continues #4

--- pkg/tareas/project.go
+++ pkg/tareas/project.go
@@ -26,19 +26,23 @@ func (p *Project) SetTags(newtags string) {
   p.Tags = strings.Fields(newtags)
 }
 
-func(p *Project) AddTask(t Task) {
+func (p *Project) AddTask(t Task) {
   p.Items = append(p.Items, t)
 }
 
-func(p *Project) RemoveTask(tpos int){
+func (p *Project) RemoveTask(tpos int){
   p.Items = append(p.Items[:tpos], p.Items[tpos:]...)
 }
 
-func(p *Project) GetAll() []Task {
+func (p *Project) GetAll() []Task {
   return p.Items
 }
 
-func(p *Project) SearchByCompleted(done bool) []Task {
+func (p *Project) Get(pos int) Task{
+  return p.Items[pos]
+}
+
+func (p *Project) SearchByCompleted(done bool) []Task {
   var tasks []Task
   for _, t := range p.Items {
     if t.Done == done {
